93d633e3ac50b6995cf94fc936bf1dfb3759170a
Obed101 <obedamoako92@gmail.com>
2021-11-02 03:51:05 -0700

    started ruby

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Searching for Holberton
+
+puts ARGV[0].scan(/Holberton/).join
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,8 @@
+# This is a Regular Expressions Folder
+
+```
+/\bObed/i
+```
+obed
+`Obed`
+OBED
