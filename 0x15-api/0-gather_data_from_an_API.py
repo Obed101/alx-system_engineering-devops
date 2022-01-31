16ab0a4a345d2515153445941d2dd5e5325e4060
Obed101 <obedamoako92@gmail.com>
2022-01-31 22:40:29 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,6 +7,10 @@ if __name__ == "__main__":
     id = int(sys.argv[1])
     url = f'https://jsonplaceholder.typicode.com/todos/{id}'
     usr_tasks = requests.get(url).json()
+    user_url = f'https://jsonplaceholder.typicode.com/?userId={id}'
+    if requests.get(user_url).status_code == 200:
+        users = requests.get(user_url).json()
+
     tasks_done = 0
     for task in usr_tasks.values():
         if task == "completed":
@@ -16,7 +20,7 @@ if __name__ == "__main__":
     def print_emp(tasks_done, usr_tasks):
         """ Prints Employees tasks done and total tasks """
         task_info = "Employee {} is done with tasks({}/{}):".format(
-                usr_tasks.get('name'), tasks_done, len(usr_tasks))
+                users.get('name'), tasks_done, len(usr_tasks))
         print(task_info)
         for task in usr_tasks.values():
             if task == "completed":
