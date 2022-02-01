f1c0b18afec9fa5f4c978a89cc0e5419ca3ecadb
Obed101 <obedamoako92@gmail.com>
2022-02-01 21:31:09 +0000

    csv file created

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 """ Exporting data to CSV fromat """
+import csv
+import requests
+import sys
+
 if __name__ == '__main__':
-    pass
+    id = sys.argv[1]
+    task_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            id)
+    tasks = requests.get(task_url).json()
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    users = requests.get(user_url).json()
+    csv_file = "{}.csv".format(id)
+
+    with open(csv_file, 'w', newline='') as file:
+        convert = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            convert.writerow(id, users.get('username'), task.get('completed'),
+                    task.get('title'))
