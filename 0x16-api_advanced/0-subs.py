545220d4b34206b135b69225c27275ffd6f0ef28
Obed101 <obedamoako92@gmail.com>
2022-02-15 11:40:38 +0000

    filtering

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,8 @@ import requests
 def number_of_subscribers(subreddit):
     """ This methos counts the number of subscribers for one sub_r """
     url = 'https://www.reddit.com/dev/api/r/{}/about'.format(subreddit)
-    n_of_subs = requests.get(url)
-
-    return n_of_subs if n_of_subs else 0
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0"}
+    n_of_subs = requests.get(url, headers=headers, allow_redirects=False)
+    print(n_of_subs.status_code)
+    return n_of_subs if n_of_subs and not n_of_subs.staus_code == 404 else 0
 
