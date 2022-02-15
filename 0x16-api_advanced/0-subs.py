ea1b1ba324a98f8df619e6e19064da24e0e3e7eb
Obed101 <obedamoako92@gmail.com>
2022-02-15 15:07:06 +0000

    subscribers in

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ This module uses flask to receive Reddit API """
 import requests
+from flask import jsonify
 
 def number_of_subscribers(subreddit):
     """ This methos counts the number of subscribers for one sub_r """
-    url = 'https://www.reddit.com/dev/api/r/{}/about'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about'.format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0"}
     n_of_subs = requests.get(url, headers=headers, allow_redirects=False)
     print(n_of_subs.status_code)
-    return n_of_subs if n_of_subs and not n_of_subs.staus_code == 404 else 0
-
+    if "{" in n_of_subs:
+        subs = n_of_subs.get("subscribers")
+        return subs if subs and not n_of_subs.status_code == 404 else 0
+    return 0
